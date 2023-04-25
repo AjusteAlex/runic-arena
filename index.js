a64f53cc82b0bef7484c385f080979e5ee8faa47
Ajustealex <alexandre.ajuste@gmail.com>
2023-04-25 13:49:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+app.listen(port, hostname, () => {
+	console.log(`Serveur demarré sur http://${hostname}:${port}`);
+});
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+  });
