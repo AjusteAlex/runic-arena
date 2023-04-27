2e31d08df68a06cd83b5c113e2cdff2af01ef000
Ajustealex <alexandre.ajuste@gmail.com>
2023-04-27 13:47:13 +0200

    [#29] Ability- Routes crud créer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 var cardsrouter = require("./routes/card");
 var skillsrouter = require("./routes/skill");
+var abilitysrouter = require("./routes/ability");
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
@@ -13,5 +14,6 @@ app.listen(port, hostname, () => {
 
 app.use("/card", cardsrouter);
 app.use("/skill", skillsrouter);
+app.use("/ability", abilitysrouter);
 
 module.exports = app;
---
+++ routes/ability.js
@@ -0,0 +1,77 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+var express = require('express');
+var router = express.Router();
+const multer = require('multer')
+const upload = multer()
+
+
+router
+  .route("/")
+  .get(async function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    const allabilitys = await prisma.ability.findMany({});
+    res.statusCode = 200;
+    res.end(JSON.stringify({ allabilitys }));
+  })
+
+  .post(upload.none(), async function (req,res){
+    try{ 
+      const ability = await prisma.ability.create({
+        data: {
+            name: req.body.name,
+            description: req.body.description,
+            value: parseInt(req.body.value),
+            state: req.body.state
+        }
+      })
+      res.status(200).json({ message: 'Capacité bien créer.'})
+    }catch(err){
+      res.status(400)
+      res.send('Erreur')
+    }
+  })
+  router
+    .get('/:id', async function (req,res){
+      const ability = await prisma.ability.findUnique({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      })
+      res.send(ability)
+    })
+    .put('/:id', upload.none(), async function (req,res){
+      try{
+        const ability = await prisma.ability.update({
+          where: {
+            id: parseInt(req.params.id),
+          },
+          data: {
+            name: req.body.name,
+            description: req.body.description,
+            value: parseInt(req.body.value),
+            state: req.body.state
+          }
+        })
+        res.status(200).json({ message: 'Capacité bien modifié.'})
+      }
+      catch(err){
+        res.status(400)
+        res.send('Erreur')
+      }
+
+    })
+    .delete('/:id', async function (req,res){
+      const ability = await prisma.ability.delete({
+        where: {
+          id: parseInt(req.params.id),
+        }
+      })
+      res.status(200).json({ message: 'Capacité supprimé.'})
+    })
+
+module.exports = router;
